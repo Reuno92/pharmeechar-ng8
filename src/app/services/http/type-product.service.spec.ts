d83aad88063f1485192aaf37f87685f4acb6bc2c
Reuno92 <racinet.renaud@orange.fr>
2019-10-14 11:55:59 +0200

    feat(Services): Create http services

---
+++ src/app/services/http/type-product.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TypeProductService } from './type-product.service';
+
+describe('TypeProductService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TypeProductService = TestBed.get(TypeProductService);
+    expect(service).toBeTruthy();
+  });
+});
