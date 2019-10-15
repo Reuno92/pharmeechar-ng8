f02b3aa9c6dcaf4f81510424387decedeb7e4f40
Reuno92 <racinet.renaud@orange.fr>
2019-10-15 13:23:48 +0200

    doc(TypeProductService): Documentation added

--- src/app/services/http/type-product.service.ts
+++ src/app/services/http/type-product.service.ts
@@ -17,15 +17,33 @@ export class TypeProductService {
     this.typeProductPath = apiPath.HOST + apiPath.PORT + '/' + apiPath.TYPE_PRODUCT;
   }
 
+  /**
+   * Get all ref for type of product
+   * @return Observable<HttpResponse<TypeProducts[]>>
+   */
   public getAllRef(): Observable<HttpResponse<TypeProducts[]>> {
     return this.http.get<TypeProducts[]>(this.typeProductPath, {
       headers : new HttpHeaders(), responseType: 'json', reportProgress: true, observe: 'response'
     });
   }
 
+  /**
+   * Get an specific type of product
+   * @Param id
+   */
   public getSingleRef(id): Observable<HttpResponse<TypeProducts>> {
     return this.http.get<TypeProducts>(this.typeProductPath + '/' + id, {
       headers: new HttpHeaders(), responseType: 'json', reportProgress: true, observe: 'response'
     });
   }
+
+  /**
+   *  Create a Ref for type of product
+   */
+  public setRef() {}
+
+  /**
+   *  Delete a Ref for type of product
+   */
+  public deleteRef() {}
 }
