d0f723b6e7dde2d715f6fe2dc2cc57a6653e1746
Reuno92 <racinet.renaud@orange.fr>
2019-10-15 16:15:27 +0200

    Feat(ProductService) getSingleProduct and GetAllProducts created

--- src/app/services/http/products.service.spec.ts
+++ src/app/services/http/products.service.spec.ts
@@ -1,14 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
 
 describe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService]
+    });
+  });
 
-/*
+
   it('should be created', () => {
     const service: ProductsService = TestBed.get(ProductsService);
     expect(service).toBeTruthy();
   });
-*/
+
+  it('should get an product equal name is Ganesh', () => {
+    inject(
+      [HttpClientTestingModule, ProductsService],
+      (httpMock: HttpTestingController, productService: ProductsService) => {
+
+          const id = 5;
+          const MOCK_PRODUCT = {
+          "id": 5,
+          "name": "Ganesh",
+          "width": 82,
+          "depth": 96,
+          "height": 101,
+          "fk_color": [3, 5, 2],
+          "type" : "office"
+        };
+
+          productService.getSingleProduct(id)
+            .subscribe( (event: HttpEvent<any>) => {
+              switch (event.type) {
+                case HttpEventType.Response:
+                  expect(event.body).toEqual(MOCK_PRODUCT);
+              }
+            });
+
+          const req = httpMock.expectOne(productService.productPath + '/' + id,
+            'return ganesh product with it id');
+            expect(req.request.method).toBe('GET');
+            expect(req.cancelled).toBeFalsy();
+            expect(req.request.responseType).toEqual('json');
+
+            req.flush(MOCK_PRODUCT);
+
+            httpMock.verify();
+        });
+  });
+
 });
--- src/app/services/http/products.service.ts
+++ src/app/services/http/products.service.ts
@@ -1,23 +1,48 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Products} from '../../models/Products';
+import {API} from '../../constant/API';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor(public http: HttpClient) {}
+  private apiPath: API;
+  public productPath: string;
+
+  constructor(public http: HttpClient) {
+    this.apiPath = new API();
+    this.productPath = this.apiPath.HOST + this.apiPath.PORT + this.apiPath.PRODUCTS;
+  }
 
   /**
    * Get one product
    * @Param id
    */
-  public getSingleProduct(id) {}
+  public getSingleProduct(id): Observable<HttpResponse<Products>> {
+    return this.http.get<Products>(this.productPath + '/' + id,
+      {
+        headers: new HttpHeaders(),
+        responseType: 'json',
+        reportProgress: true,
+        observe: 'response'
+      });
+  }
 
   /**
    * Get All products
    */
-  public getAllProducts() {}
+  public getAllProducts(): Observable<HttpResponse<Products[]>> {
+    return this.http.get<Products[]>(this.productPath,
+      {
+        headers: new HttpHeaders(),
+        responseType: 'json',
+        reportProgress: true,
+        observe: 'response'
+      });
+  }
 
   /**
    * Get product by different filter
